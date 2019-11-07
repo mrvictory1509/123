bf8b4607de606d16dd40313439e9d4b25b891a6b
mrvictory1509 <55972167+mrvictory1509@users.noreply.github.com>
2019-11-07 09:56:25 +0700

    Update ATNdetail.php

--- ATNdetail.php
+++ ATNdetail.php
@@ -10,10 +10,10 @@
 	<div class="header">
 			<div class="nava">
 			<ul>
-				<li><a href="https://designweb.herokuapp.com/ATN.php">Trang chủ</a></li>
-				<li><a href="">Kiểm tra đơn hàng</a></li>
-				<li><a href="">Đăng nhập</a></li>
-				<li><a href="https://designweb.herokuapp.com/Dangkykh.php">đăng ký</a></li>
+				<li><a href="https://designweb.herokuapp.com/ATN.php">Home</a></li>
+				<li><a href="">Check Order</a></li>
+				<li><a href="">Log in</a></li>
+				<li><a href="https://designweb.herokuapp.com/Dangkykh.php">Register</a></li>
 				<li><a href="https://designweb.herokuapp.com/admin.php">Admin</a></li>
 			</ul>
 			</div>
@@ -39,7 +39,6 @@
 		            $querycategory = "SELECT categoryid, categoryname FROM category";
 		            $total = pg_query($connection,$querycategory);
 		            if (pg_num_rows($total) > 0) {
-		            // output data of each row
 		            while($rowcategory = pg_fetch_assoc($total)) {
 		              $categoryid = $rowcategory['categoryid'];
 		              $categoryname = $rowcategory['categoryname'];
@@ -55,7 +54,6 @@
 		            $querycategory = "SELECT * FROM category WHERE categoryid = $categoryid";
 		            $total = pg_query($connection,$querycategory);
 		            if (pg_num_rows($total) > 0) {
-		            // output data of each row
 		            while($rowcategory = pg_fetch_assoc($total)) {
 		              $categoryid = $rowcategory['categoryid'];
 		              $categoryname = $rowcategory['categoryname'];
@@ -66,8 +64,6 @@
 					}}
 				?>
 		</div>
-
-
 		<div >
 			<br>
 		<?php
@@ -105,16 +101,16 @@
 	<div class="footer">
 		<table  cellspacing="0" cellpadding="10" width= 100% align="center" >
 			<tr >
-			<th style="font-size: 17px; color:#FFFFFF"  >ATN_KÊNH MUA SẮM & DỊCH VỤ TRỰC TUYẾN HÀNG ĐẦU VIỆT NAM!</th>
-			<th  rowspan="2" style=" color:#FFFFFF" > CÔNG TY TNHH ATN <br>
- 													Giấy CNĐKDN: 289037490 – Ngày cấp: 06/5/2005, được sửa đổi lần thứ 17 ngày 24/7/2017. <br>
- 													Cơ quan cấp: Phòng Đăng ký kinh doanh – Sở kế hoạch và Đầu tư hà Nội. <br>
- 													Địa chỉ đăng ký kinh doanh: Tầng 71, Tòa Nhà Keangnam, E6, Phạm Hùng, Phường Mễ Trì, Quận Nam Từ Liêm, Hà Nội, Việt Nam <br>  <br><br>     @ATN 2019
+			<th style="font-size: 17px; color:#FFFFFF"  >ANT_ TOP CHANNELS TO SHOP ONLINE!!!</th>
+			<th  rowspan="2" style=" color:#FFFFFF" > ATN CO., LTD <br>
+ 													Business registration certificate: 245638792 - Date of issue: Oct 10, 2015, amended for the 9th time on Mar 15, 2019.<br>
+ 													Issuing agency: Business Registration Office - Hanoi Department of Planning and Investment.  <br>
+ 													Registered business address: 2nd Floor, 152 Nguyen Dinh Hoan, Cau Giay, Hanoi, Vietnam  <br>  <br><br>    @ATN 2019
  			</th>
 			</tr>
 			<tr >
 				<td ><div align="center" style="padding-top:0%, width= 20px; color:#FFFFFF" >	
-				</style>Mua hàng trực tuyến (mua hàng online) mang lại sự tiện lợi, lựa chọn đa dạng hơn và các dịch vụ tốt hơn cho người tiêu dùng, thế nhưng người tiêu dùng Việt Nam vẫn chưa tận hưởng được những tiện ích đó. Chính vì vậy ATN Việt Nam được triển khai với mong muốn trở thành trung tâm mua sắm trực tuyến số 1 tại Việt Nam, nơi bạn có thể chọn lựa mọi thứ để chăm sóc thứ đồ chơi bạn yêu thích.... Chúng tôi có tất cả!</div></td>	
+				</style>Buying goods online brings convenience, more diverse options and better services to consumers. That's why ATN Vietnam was launched with the desire to become the number 1 online shopping center in Vietnam, where you can choose everything to take care of your favorite toys .... All there in us!</div></td>	
 			</tr>
 		</table>
 	</div>
